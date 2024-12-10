c3179aa46920c6e2c9a0891dd31fdf2a0e32d9d2
Devan Neil <devanneil@mines.edu>
2024-12-09 17:59:43 -0700

    Added a description to qFile so that I can make Courage implement it

--- src/core/lib/qFile/qFile.h
+++ src/core/lib/qFile/qFile.h
@@ -1,9 +1,21 @@
 #ifndef QFILE_H
 #define QFILE_H
 enum FileType {
-    FOLDER, DATA, NONE, CLOSED, WRITE
+    FOLDER, DATA, NONE, CLOSED, WRITE, APPEND
 };
-
+/**
+ * Cannot be bothered to make this right now, might make Courage do it unsure yet
+ * 
+ * qFile will handle all fileIO operations in quantum engine. a qFile object can be any object in the OS file system.
+ * FileType specifies which type it is. It has to be one of the file types and cannot be more than one file type. A data
+ * file for instance cannot be a folder. There is some hierarchy as a data file can become a write or append file
+ * 
+ * qFile init() will also be important as it will find the path to the home directory. We will use the C file directory
+ * system and the C file management system. In quantumEngine proper qFile will run in it's own process so if quantumEngine
+ * crashes we can still close all the relevent files
+ * 
+ * qFile will also handle our logger class once I figure out what I want to do with that
+ */
 class qFile {
     private:
     unsigned int size;
